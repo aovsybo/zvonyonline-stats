1c1469f2b6148780679d4620af704e75df9f07e2
matvey <matgr2n@gmail.com>
2024-01-17 19:41:32 +0400

    changed to async

--- src/integrations/api/views.py
+++ src/integrations/api/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+from django.conf import settings
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,15 +8,13 @@ from rest_framework import status
 from .serializers import CallInfoSerializer
 from integrations.models import CallInfo
 from integrations.service.skorozvon import (
-    get_call,
-    get_calls,
-    get_scenarios,
-    get_scenario,
+    SkorozvonAPI
 )
 from integrations.service.google_sheets import (
     create_main_sheet_copy,
     get_project_indexes,
     write_project_stat_to_google_sheet,
+    get_table_data
 )
 
 
@@ -31,12 +30,17 @@ class WriteDataToGoogleSheet(ListAPIView):
     queryset = CallInfo.objects.all()
 
     def get(self, request):
-        sheet_name = (datetime.utcnow() + timedelta(hours=4)).strftime("%H:%M")
-        create_main_sheet_copy(sheet_name)
-        # TODO: async
-        write_project_stat_to_google_sheet(
-            sheet_name=sheet_name,
-            db_projects_info=self.serializer_class(self.get_queryset(), many=True).data,
-            project_indexes=get_project_indexes(),
-        )
-        return Response(status=status.HTTP_200_OK)
+        skorozvon = SkorozvonAPI()
+        data = skorozvon.get_data()
+        return Response(data=data, status=status.HTTP_200_OK)
+
+    # def get(self, request):
+    #     sheet_name = (datetime.utcnow() + timedelta(hours=4)).strftime("%H:%M")
+    #     create_main_sheet_copy(sheet_name)
+    #     # TODO: async
+    #     write_project_stat_to_google_sheet(
+    #         sheet_name=sheet_name,
+    #         db_projects_info=self.serializer_class(self.get_queryset(), many=True).data,
+    #         project_indexes=get_project_indexes(),
+    #     )
+    #     return Response(status=status.HTTP_200_OK)
--- src/integrations/service/skorozvon.py
+++ src/integrations/service/skorozvon.py
@@ -1,47 +1,84 @@
 import requests
+import aiohttp
+import asyncio
+
+from datetime import datetime, timedelta
+import time
 
 from django.conf import settings
 
 
-def get_token():
-    url = "https://app.skorozvon.ru/oauth/token"
-    data = {
-        "grant_type": "password",
-        "username": settings.SKOROZVON_LOGIN,
-        "api_key": settings.SKOROZVON_API_KEY,
-        "client_id": settings.SKOROZVON_APPLICATION_ID,
-        "client_secret": settings.SKOROZVON_APPLICATION_KEY,
-    }
-    token = requests.post(url, data=data).json()
-    return f"Bearer {token['access_token']}"
+class SkorozvonAPI:
+    API_URL = f"https://app.skorozvon.ru/api/v2/"
+    _token = None
 
+    def __init__(self):
+        self._token = self.get_token()
 
-def get_request(sub_url: str, params: dict = None):
-    headers = {
-        "Authorization": get_token()
-    }
-    response = requests.get(url=f"https://app.skorozvon.ru/api/v2/{sub_url}", headers=headers, params=params)
-    return response.json()
+    def get_token(self):
+        token_url = "https://app.skorozvon.ru/oauth/token"
+        data = {
+            "grant_type": "password",
+            "username": settings.SKOROZVON_LOGIN,
+            "api_key": settings.SKOROZVON_API_KEY,
+            "client_id": settings.SKOROZVON_APPLICATION_ID,
+            "client_secret": settings.SKOROZVON_APPLICATION_KEY,
+        }
+        response = requests.post(token_url, data=data).json()
+        return f"Bearer {response['access_token']}"
 
+    def get_request(self, sub_url: str, params: dict = None):
+        return requests.get(
+            url=f"{self.API_URL}{sub_url}",
+            headers={"Authorization": self._token},
+            params=params
+        ).json()
 
-def get_call(call_id: int):
-    return get_request(f"calls/{call_id}")
+    async def get_async_request(self, session, sub_url: str, params: dict = None):
+        async with session.get(
+                url=f"{self.API_URL}{sub_url}",
+                headers={"Authorization": self._token},
+                params=params
+        ) as response:
+            return await response.json(content_type=None)
 
+    def get_projects_ids(self):
+        response = self.get_request(
+            sub_url="call_projects",
+            params={"length": 100},
+        )
+        return [
+            project["id"]
+            for project in response["data"]
+            if project["title"] in settings.GS_TO_SKOROZVON_NAME.values()
+        ]
 
-def get_scenarios():
-    return get_request("scenarios")
+    async def get_one_project_stat_by_id(self, session, project_id: str, start_time: int):
+        return await self.get_async_request(
+            session=session,
+            sub_url=f"call_projects/{project_id}/statistic",
+            params={"start_time": start_time},
+        )
 
+    def get_calls(self, project_id: int, start_time: int):
+        params = {
+            "page": 1,
+            "length": 100,
+            "sort_by_time": True,
+            "start_time": start_time,
+            "sort_direction": "DESC",
 
-def get_scenario(scenario_id: str):
-    return get_request(f"scenarios/{scenario_id}")
+        }
+        return self.get_request("calls", params)
 
+    async def get_projects_stat(self, start_time: int):
+        async with aiohttp.ClientSession() as session:
+            tasks = [
+                asyncio.create_task(self.get_one_project_stat_by_id(session, project_id, start_time))
+                for project_id in self.get_projects_ids()
+            ]
+            return await asyncio.gather(*tasks)
 
-def get_calls():
-    params = {
-        "page": 1,
-        "length": 100,
-        "sort_by_time": True,
-        "start_time": 1705185193,
-        "sort_direction": "DESC"
-    }
-    return get_request("calls", params)
+    def get_data(self):
+        start_time = int(time.mktime((datetime.now() - timedelta(hours=1)).timetuple()))
+        return asyncio.run(self.get_projects_stat(start_time))
