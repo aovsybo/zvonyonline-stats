e3fe3f915c1f36b72f12ea9e8b64d8dbffafaef7
matvey <matgr2n@gmail.com>
2024-01-23 13:56:01 +0300

    added new models

--- src/integrations/models.py
+++ src/integrations/models.py
@@ -1,10 +1,3 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
@@ -20,3 +13,31 @@ class Leads(models.Model):
     class Meta:
         managed = False
         db_table = 'Leads'
+
+
+class QualifiedLeads(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    phoneNumber = models.CharField(max_length=255, blank=True, null=True)
+    site = models.CharField(max_length=255, blank=True, null=True)
+    comment = models.CharField(max_length=255, blank=True, null=True)
+    projectId = models.CharField(max_length=100, blank=True, null=True)
+    addDate = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'QualifiedLeads'
+
+
+class Dialogs(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    phoneNumber = models.CharField(max_length=255, blank=True, null=True)
+    site = models.CharField(max_length=255, blank=True, null=True)
+    comment = models.CharField(max_length=255, blank=True, null=True)
+    projectId = models.CharField(max_length=100, blank=True, null=True)
+    addDate = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Dialogs'
