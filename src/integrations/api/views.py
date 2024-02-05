19503924860076123b702f90ca8e48c2eb2feb57
matvey <matgr2n@gmail.com>
2024-02-05 12:17:55 +0300

    changed updating time

--- src/integrations/api/views.py
+++ src/integrations/api/views.py
@@ -1,11 +1,11 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
 from .serializers import LeadsSerializer
 
 
-class WriteDataToGoogleSheet(ListAPIView):
+class WriteDataToGoogleSheet(CreateAPIView):
     serializer_class = LeadsSerializer
 
     def post(self, request, *args, **kwargs):
